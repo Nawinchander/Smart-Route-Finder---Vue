bbf8878d5c5f76b72eaca0c3a0d440b0e6987807
Nawin chander <nawinnapzy289@gmail.com>
2026-05-04 11:41:38 +0530

    add

---
+++ frontend/Smart route finder/src/Components/routeForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form">
+    <input v-model="start" placeholder="Start (A-E)" />
+    <input v-model="end" placeholder="End (A-E)" />
+
+    <button @click="handleSubmit">Find Route</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["onSearch"]);
+
+const start = ref("");
+const end = ref("");
+
+const handleSubmit = () => {
+  if (!start.value || !end.value) return;
+  emit("onSearch", { start: start.value, end: end.value });
+};
+</script>
+
+
