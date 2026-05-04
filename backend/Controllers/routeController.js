bcfc753061e4783b0b68dfa7eab8d84bc1d88567
Nawin chander <nawinnapzy289@gmail.com>
2026-05-04 11:32:47 +0530

    add

---
+++ backend/Controllers/routeController.js
@@ -0,0 +1,18 @@
+const Graph = require("../dsa/Graph");
+
+const graph = new Graph();
+
+// sample map
+["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
+
+graph.addEdge("A", "B", 5);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "D", 1);
+graph.addEdge("C", "D", 7);
+graph.addEdge("D", "E", 3);
+
+exports.getRoute = (req, res) => {
+  const { start, end } = req.query;
+  const result = graph.dijkstra(start, end);
+  res.json(result);
+};
