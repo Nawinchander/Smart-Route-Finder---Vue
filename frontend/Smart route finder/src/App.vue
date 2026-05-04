4e291eecc21745f7e9011e93bf956c4c180c6039
Nawin chander <nawinnapzy289@gmail.com>
2026-05-04 11:46:57 +0530

    add

--- frontend/Smart route finder/src/App.vue
+++ frontend/Smart route finder/src/App.vue
@@ -1,11 +1,17 @@
 <script setup></script>
 
-<template>
+<!-- <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
-</template>
+</template> -->
 
 <style scoped></style>
+
+
+<template>
+  <router-view />
+</template>
+
