d67c46f7cea38cc26689d0c6a849e01ee8b326d9
Nawin chander <nawinnapzy289@gmail.com>
2026-05-04 11:42:33 +0530

    add

---
+++ frontend/Smart route finder/src/Components/routeResult.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="result" class="result">
+    <h3>Shortest Path</h3>
+    <p>{{ result.path.join(" → ") }}</p>
+    <p><strong>Distance:</strong> {{ result.distance }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  result: Object
+});
+</script>
