9fa8392dbae0ad0023498b801d1d1cbb61a37f74
Nawin chander <nawinnapzy289@gmail.com>
2026-05-05 15:54:18 +0530

    add

--- frontend/Smart route finder/src/App.vue
+++ frontend/Smart route finder/src/App.vue
@@ -1,4 +1,24 @@
+<template>
+  <h1>Smart Route Finder</h1>
+  <RouteForm @onSearch="fetchRoute" />
+  <RouteResult :result="result" />
+  <LiveUpdates />
+</template>
 
+<script setup>
+import { ref } from "vue";
+import { getRoute } from "./api/routeService";
+
+import RouteForm from "./Components/RouteForm.vue";
+import RouteResult from "./Components/RouteResult.vue";
+import LiveUpdates from "./Components/LiveUpdates.vue";
+
+const result = ref(null);
+
+const fetchRoute = async ({ start, end }) => {
+  result.value = await getRoute(start, end);
+};
+</script>
 
 <!-- <script setup></script>
 
