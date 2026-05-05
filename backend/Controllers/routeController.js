7077fecd9ca322652b623adafaff9c3029398e95
Nawin chander <nawinnapzy289@gmail.com>
2026-05-05 15:48:35 +0530

    add

--- backend/Controllers/routeController.js
+++ backend/Controllers/routeController.js
@@ -1,8 +1,8 @@
 const Graph = require("../dsa/Graph");
+const { sendRouteUpdate } = require("../websocket/socket");
 
 const graph = new Graph();
 
-// sample map
 ["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
 
 graph.addEdge("A", "B", 5);
@@ -13,6 +13,35 @@ graph.addEdge("D", "E", 3);
 
 exports.getRoute = (req, res) => {
   const { start, end } = req.query;
+
   const result = graph.dijkstra(start, end);
+
+  // 🔥 emit realtime update
+  sendRouteUpdate({
+    start,
+    end,
+    ...result
+  });
+
   res.json(result);
 };
+
+
+// const Graph = require("../dsa/Graph");
+
+// const graph = new Graph();
+
+// // sample map
+// ["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
+
+// graph.addEdge("A", "B", 5);
+// graph.addEdge("A", "C", 2);
+// graph.addEdge("B", "D", 1);
+// graph.addEdge("C", "D", 7);
+// graph.addEdge("D", "E", 3);
+
+// exports.getRoute = (req, res) => {
+//   const { start, end } = req.query;
+//   const result = graph.dijkstra(start, end);
+//   res.json(result);
+// };
