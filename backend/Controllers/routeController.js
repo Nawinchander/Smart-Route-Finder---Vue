1943ad55268938085840c8f027a18cc6576a4ee6
Nawin chander <nawinnapzy289@gmail.com>
2026-05-06 20:03:32 +0530

    add

--- backend/Controllers/routeController.js
+++ backend/Controllers/routeController.js
@@ -1,37 +1,25 @@
-const Graph = require("../dsa/Graph");
-const { sendRouteUpdate } = require("../websocket/socket");
+const routeQueue = require("../queue/routeQueue");
 
-const graph = new Graph();
-
-["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
-
-graph.addEdge("A", "B", 5);
-graph.addEdge("A", "C", 2);
-graph.addEdge("B", "D", 1);
-graph.addEdge("C", "D", 7);
-graph.addEdge("D", "E", 3);
-
-exports.getRoute = (req, res) => {
+exports.getRoute = async (req, res) => {
   const { start, end } = req.query;
 
-  const result = graph.dijkstra(start, end);
+  // add job to queue
+  const job = await routeQueue.add({ start, end });
 
-  // 🔥 emit realtime update
-  sendRouteUpdate({
-    start,
-    end,
-    ...result
-  });
+  // wait for result (or you can return jobId for async polling)
+  const result = await job.finished();
 
   res.json(result);
 };
 
 
+
+
 // const Graph = require("../dsa/Graph");
+// const { sendRouteUpdate } = require("../websocket/socket");
 
 // const graph = new Graph();
 
-// // sample map
 // ["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
 
 // graph.addEdge("A", "B", 5);
@@ -42,6 +30,35 @@ exports.getRoute = (req, res) => {
 
 // exports.getRoute = (req, res) => {
 //   const { start, end } = req.query;
+
 //   const result = graph.dijkstra(start, end);
+
+//   // 🔥 emit realtime update
+//   sendRouteUpdate({
+//     start,
+//     end,
+//     ...result
+//   });
+
 //   res.json(result);
 // };
+
+
+// // const Graph = require("../dsa/Graph");
+
+// // const graph = new Graph();
+
+// // // sample map
+// // ["A", "B", "C", "D", "E"].forEach(n => graph.addNode(n));
+
+// // graph.addEdge("A", "B", 5);
+// // graph.addEdge("A", "C", 2);
+// // graph.addEdge("B", "D", 1);
+// // graph.addEdge("C", "D", 7);
+// // graph.addEdge("D", "E", 3);
+
+// // exports.getRoute = (req, res) => {
+// //   const { start, end } = req.query;
+// //   const result = graph.dijkstra(start, end);
+// //   res.json(result);
+// // };
